c0b3db747c225874f846469f1249dd3f6a06c572
Christian Kirschberg <ckirschberg@gmail.com>
2021-09-26 20:05:32 +0200

    initial

---
+++ src/Run.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Run
+{
+	public static void main(String[] args)
+	{
+		//1: Create 4 person objects.
+		//person 1: Jens, 175 cm, 69 kg.
+		//person 2: Helle, 168 cm, 58 kg
+		//person 3: Berit, 163 cm, 72 kg
+		//person 4: Allan, 192 cm, 95 kg
+
+		
+
+		//2: Create a Person array with 4 positions.
+		//Populate the array with the 4 person objects
+		
+		
+		//3: Using System.out.println(), print out the names and heights and weights of the 4 people.
+		// Note: It must work without modification if you changed the size of the array and the number of
+		// person objects in it.
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	//4: Write a static method that returns the height of the specified name. The specified name
+	//is sent to the method as a parameter. The Person array to search in is also sent as a parameter.
+	//If the specified name does not exist in the array, the method should return 0.
+
+
+
+
+
+	
+	//5: Write a static method that returns the tallest person object in a given person array.
+	//The method must take in a person array as a parameter.
+	//The method should return null if the array is empty. (no people objects in it).
+	
+	
+	
+	//6: write a static method that returns the first person object in the array, that weighs more and is taller than x and y.
+	//x and y should be parameters to the method and the person array to search in as well.
+	//The method should return null if the array is empty. (no people objects in it).
+	
+	
+	//7: write a method like the one above, but this time do not return only the first person object that fits the criteria.
+	//Instead return all objects that fits the criteria. The method must work also if you changed the array and number of 
+	//people in it.
+}
